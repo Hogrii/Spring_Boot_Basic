48de7c874a1f6dc5c7cfef5e30a0261a383da1ec
DESKTOP-A922TNL\LeeJinho <hosboy93@naver.com>
2023-03-07 16:09:32 +0900

    회원 도메인 개발 - 등급 - 회원 가입, 조회

---
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+
+    // Alt + Enter 누르면 MemberRepository(Interface)에 있는 메소드를 가져온다.
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
